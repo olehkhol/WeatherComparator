0d72b6e2b85ad09e728a95252ed1f36479a5ac68
Oleh Kholiavchuk <oleh.khol@gmail.com>
2021-04-15 21:53:51 +0300

    Unnecessary dependencies for testing have been removed AndroidX navigation-fragment updated 2.3.5 AndroidX navigation-ui updated 2.3.5 AndroidX recyclerview updated 1.2.0

---
+++ app/src/androidTest/java/ua/in/khol/oleh/touristweathercomparer/ExampleInstrumentedTest.kt
@@ -0,0 +1,18 @@
+package ua.`in`.khol.oleh.touristweathercomparer
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    fun useAppContext() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("ua.in.khol.oleh.touristweathercomparer", appContext.packageName)
+    }
+}
